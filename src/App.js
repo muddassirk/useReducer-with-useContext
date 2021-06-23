64cdbb5f1d581070a12c357f44f36996d38cfc14
muddassirk <mkkhanu72@gmail.com>
2021-06-24 00:31:42 +0500

    complete counter

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react'
+import ComponentA from './components/ComponentA'
+import './App.css';
+// import Counter from './components/Counter';
+export const CounterContext = React.createContext()
+
+const initialState = {
+    counter: 0
+}
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case "increment":
+            return {...state, counter: state.counter + action.payload }
+        case "decrement":
+            return {...state, counter: state.counter - action.payload }
+        case "reset":
+            return initialState
+        default:
+            return state
+    }
+}
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+    return ( 
+        <CounterContext.Provider value={{counter: state.counter, dispatch}}>
+          <div className="App">
+              App.js Counter: {state.counter}
+              <ComponentA />
+          </div>
+        </CounterContext.Provider>
+    );
+}
+
+export default App;
